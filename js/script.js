c2791291569291eeb032791f6ed7b615b5190b4e
ktscates <cates2506@gmail.com>
2019-07-20 13:44:22 +0200

    Declaring variables in js file.

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// Declaring variables for female and male names
+var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
