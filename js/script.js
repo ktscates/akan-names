1e06e87dd62d61607eaf17b30557aa081f5cb38f
ktscates <cates2506@gmail.com>
2019-07-20 13:56:04 +0200

    declare date and calcute the century.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-// Declaring variables for female and male names
+// Declaring variables for female and male names.
 var femaleNames=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 var maleNames=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+
+// Create a function that will be called whenever you hit the submit button.
+function akanNames(){
+
+    // Declare date inputs.
+    var DD=parseFloat(document.getElementById("dd").value);
+    var MM=parseFloat(document.getElementById("mm").value);
+    var YY=parseFloat(document.getElementById("yy").value);
+
+    // calculate the century.
+    var CC=(YY-1)/100+1;
+
+    
+}
